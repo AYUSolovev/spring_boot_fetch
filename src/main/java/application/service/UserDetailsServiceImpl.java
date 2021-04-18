ec38e0a4137919bcaa96fea46e8f3e6f0330fd55
Solovev Anatoliy <tolya-ptisa@mail.ru>
2021-04-18 11:37:31 +0300

    project transition from spring to spring boot

---
+++ src/main/java/application/service/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package application.service;
+
+import application.repository.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+
+    private UserDao userDao;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return userDao.loadUserByUsername(s);
+    }
+}
